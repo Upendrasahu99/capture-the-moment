a85d3584f2db7ddf85d13df726d9fa87cc19dc5d
Upendra Sahu <upendrasahu1199@gmail.com>
2024-09-10 00:05:42 +0530

    changed color of sidebar text and added logo in sidebar

--- src/js/home.js
+++ src/js/home.js
@@ -1,11 +1,12 @@
+// navbar ===============
 // select element
 const sideBar = document.querySelector('.sidebar');
 const sideBarBtn = document.querySelector('.open-close-sidebar');
 
-console.log(sideBar);
-
 sideBarBtn.addEventListener('click', (e) => {
     sideBar.classList.toggle('left-full');
     sideBarBtn.innerHTML = sideBar.classList.contains('left-full') ? `<i class='bx bx-menu' ></i>` : `<i class='bx bx-x' ></i>`;
-    sideBarBtn.classList.toggle('text-white')
 })
+
+//=============================
+
