25435b3d2e93f0a3245ccbd7fc7e73e89a87de5d
Upendra Sahu <upendrasahu1199@gmail.com>
2024-09-10 08:02:46 +0530

    Added other pages
    - Created other pages with shared navbar and shared js file for navbar
    
    - Added page link in navbar for navigation

--- src/js/home.js
+++ src/js/home.js
@@ -1,18 +1,9 @@
-// navbar ===============
-// select element
-const sideBar = document.querySelector('.sidebar');
-const sideBarBtn = document.querySelector('.open-close-sidebar');
-
-sideBarBtn.addEventListener('click', (e) => {
-    sideBar.classList.toggle('left-full');
-    sideBarBtn.innerHTML = sideBar.classList.contains('left-full') ? `<i class='bx bx-menu' ></i>` : `<i class='bx bx-x' ></i>`;
-})
-
 //=============================
 
 //center animated text
 
-const txt = 'love · moments · memories';
+// const txt = 'love · moments · memories';
+const txt = 'प्यार · पल · यादें';
 
 var i = 0;
 var speed = 100;
