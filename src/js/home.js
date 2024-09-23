d03f6b26692e5d7c15aba28c609d56f6d3d5a2ba
Upendra Sahu <upendrasahu1199@gmail.com>
2024-09-24 00:38:43 +0530

    Added section 11 with changing user feedback after time interval

--- src/js/home.js
+++ src/js/home.js
@@ -50,13 +50,45 @@ ytVideoSecPlayBtn.addEventListener('click', (e) => {
   ytVideoSecVideo.src +='?autoplay=1'; 
 })
 
+//section 11
+
+const feedbackData = [
+  {
+    paragraph: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    Name: '-John Doe & Jane Smith',
+    place: 'Location A'
+  },
+  {
+    paragraph: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    Name: '-Alice Johnson & Bob Brown',
+    place: 'Location B'
+  },
+  {
+    paragraph: 'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio, et feugiat tellus malesuada et. Sed lectus. Praesent elementum hendrerit tortor.',
+    Name: '-Charlie White & Dana Green',
+    place: 'Location C'
+  }
+];
 
 
+const section11 = document.querySelector('#section-11');
 
+let j = 0;
+function addData() {
+  section11.insertAdjacentHTML('beforeend', `
+    <h1 class="text-5xl">some kind words</h1>
+    <p class="w-3/5 text-2xl py-10 text-justify">${feedbackData[j].paragraph}</p>
+    <h4>${feedbackData[j].Name}</h4>
+    <h6>${feedbackData[j].place}</h6>
+  `);
+    j++;
+    if(j >= feedbackData.length){
+      j = 0;
+    }
+}
 
-
-
-
-
-
-console.log('hello world');
+addData();
+setInterval(() => {
+  section11.innerHTML= '';
+  addData();
+}, 4000);
